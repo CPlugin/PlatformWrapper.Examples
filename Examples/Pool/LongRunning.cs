d0ab3ea258e01848b35d48db6f7985485f68801a
Vladislav Sorokin <vlad@vsorokin.com>
2017-09-29 14:47:21 +0300

    unneded code has been removed

--- Examples/Pool/LongRunning.cs
+++ Examples/Pool/LongRunning.cs
@@ -17,27 +17,16 @@ namespace Examples.Pool
             {
                 Logger = Extensions.LogToConsole
             };
+
+            // very important step to run pool
             pool.Run();
 
-            /*
-             * 64th
-             * 1: 1.08
-             * 2: 0.98
-             * 4: 1.11
-             * 8: 1.30
-             * 64: 1.15
-             */
-
             var tasks = new Task[ThreadsCount];
-
-            //var symbols = new Queue<ConSymbol>();
-            //var logins = new Queue<int>();
 
             var log = LogManager.GetCurrentClassLogger();
             log.Info("Started");
 
 
-            var started = DateTime.Now;
             for (int i = 0; i < ThreadsCount; ++i)
             {
                 tasks[i] = new Task(a =>
@@ -62,17 +51,6 @@ namespace Examples.Pool
                                     _log.Info("Fetched {0} users", allUsers.Count);
                                     _log.Info("Fetched {0} trades", allTrades.Count);
                                     _log.Info("Fetched {0} symbols", symbols.Count);
-
-                                    if (false)
-                                    {
-                                        // v.Recreate(); // init reconnect routine
-                                        mgr = v.Data.Value;
-                                        allUsers = mgr.UsersRequest();
-                                        foreach (var userRecord in allUsers)
-                                        {
-                                            mgr.TradesUserHistory(userRecord.Key, DateTime.MinValue, DateTime.MaxValue);
-                                        }
-                                    }
                                 }
                             }
                             catch (TimeoutException to)
@@ -90,12 +68,10 @@ namespace Examples.Pool
                 }, i);
                 tasks[i].Start();
             }
-            log.Info("All tasks has started");
+            log.Info("All tasks started");
 
             Task.WaitAll(tasks, TimeSpan.FromHours(1));
-            log.Info("All tasks has completed");
-
-            log.Info("Total time : {0}", DateTime.Now - started);
+            log.Info("All tasks completed");
         }
     }
 }
