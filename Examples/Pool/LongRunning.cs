bcdd9df8f8760e31d0bedb2823745d988771fa73
Vladislav Sorokin <vlad@vsorokin.com>
2017-10-10 14:19:55 +0300

    little refactoring

--- Examples/Pool/LongRunning.cs
+++ Examples/Pool/LongRunning.cs
@@ -17,14 +17,18 @@ namespace Examples.Pool
         {
             Log.Info("Started");
 
+            var poolLog = LogManager.GetLogger("Pool");
             var pool = new ManagerPool(Constants.Server, Constants.Login, Constants.Password)
             {
-                Logger = Extensions.LogToConsole
+                Logger = (ctx, type, message, exception) => poolLog.Trace($"{type} {message} {exception}")
             };
+
+            var lastOpDateTime = DateTime.MinValue;
+            var rnd = new Random();
 
             // very important step to run pool
             pool.Run();
-            
+
             for (int i = 0; i < ThreadsCount; ++i)
             {
                 _tasks[i] = new Task(a =>
@@ -34,35 +38,38 @@ namespace Examples.Pool
                         var _log = LogManager.GetLogger("Thread_" + a);
                         try
                         {
-                            _log.Debug("Started");
 
                             try
                             {
                                 using (var v = pool.Get(TimeSpan.FromSeconds(5)))
                                 {
-                                    _log.Trace("Trying to get data");
-                                    var mgr = v.Data.Value;
-                                    var allUsers = mgr.UsersRequest();
-                                    var allTrades = mgr.TradesRequest();
-                                    var symbols = mgr.SymbolsGetAll();
+                                    // randomly await from 1 to 5 minutes
+                                    if (DateTime.Now - lastOpDateTime > TimeSpan.FromMinutes(rnd.Next(1, 5)))
+                                    {
+                                        lastOpDateTime = DateTime.Now;
 
-                                    _log.Info("Fetched {0} users", allUsers.Count);
-                                    _log.Info("Fetched {0} trades", allTrades.Count);
-                                    _log.Info("Fetched {0} symbols", symbols.Count);
+                                        _log.Trace("Trying to get data");
+                                        var mgr = v.Data.Value;
+                                        var allUsers = mgr.UsersRequest();
+                                        var allTrades = mgr.TradesRequest();
+                                        var symbols = mgr.SymbolsGetAll();
+
+                                        _log.Info("Fetched {0} users", allUsers.Count);
+                                        _log.Info("Fetched {0} trades", allTrades.Count);
+                                        _log.Info("Fetched {0} symbols", symbols.Count);
+                                    }
                                 }
                             }
                             catch (TimeoutException to)
                             {
                                 _log.Warn(to.Message);
                             }
-
-                            _log.Debug("Complete");
                         }
                         catch (Exception ex)
                         {
                             _log.Error(ex);
                         }
-                    } while (false == _eventStop.WaitOne(TimeSpan.FromMinutes(1)));
+                    } while (false == _eventStop.WaitOne(TimeSpan.FromSeconds(1)));
                 }, i);
                 _tasks[i].Start();
             }
